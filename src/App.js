9884635e3a613b98999b087711b54158c83b6346
AleksHodur <hxl82516@educastur.es>
2024-02-03 14:47:07 +0100

    auth context

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import BookList from "./components/BookList";
 import Navbar from "./components/Navbar";
 import ThemeToggle from "./components/ThemeToggle";
+import AuthcontextProvider from "./contexts/AuthContext";
 import ThemeContextProvider from "./contexts/ThemeContext";
 
 //sum comment
@@ -8,9 +9,11 @@ function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        <Navbar />
-        <BookList />
-        <ThemeToggle />
+        <AuthcontextProvider>
+          <Navbar />
+          <BookList />
+          <ThemeToggle />
+        </AuthcontextProvider>
       </ThemeContextProvider>
     </div>
   );
