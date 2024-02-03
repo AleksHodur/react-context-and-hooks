f34b6951d510b577b1521be6cbec2bf9bdbad78e
AleksHodur <hxl82516@educastur.es>
2024-02-03 14:59:30 +0100

    cosuming auth

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,39 @@
 import React, { Component } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
+import { AuthContext } from '../contexts/AuthContext';
 
 class Navbar extends Component {
 
     render() { 
 
         return (
-            <ThemeContext.Consumer >{(context) => {
 
-                const { isLightTheme, light, dark } = context;
-                const theme = isLightTheme ? light : dark;
+            <AuthContext>{(authContext) => (
+                <ThemeContext.Consumer >{(themeContext) => {
 
-                return(
-                    <nav style={{background: theme.ui, color: theme.text}}>
-                        <h1>Context App</h1>
-                        <ul>
-                        <li>Home</li>
-                        <li>About</li>
-                        <li>Contact</li>
-                        </ul>
-                    </nav>
-                );
-            }}
-            </ThemeContext.Consumer>
+                    const { isAuthenticated, toggleAuth } = authContext;
+                    const { isLightTheme, light, dark } = themeContext;
+                    const theme = isLightTheme ? light : dark;
+    
+                    return(
+                        <nav style={{background: theme.ui, color: theme.text}}>
+                            <h1>Context App</h1>
+                            <div onClick={toggleAuth}>
+                                { isAuthenticated ? 'Logged in' : 'Logged out'}
+                            </div>
+                            <ul>
+                                <li>Home</li>
+                                <li>About</li>
+                                <li>Contact</li>
+                            </ul>
+                        </nav>
+                    );
+                }}
+                </ThemeContext.Consumer>
+            )}
+                
+            </AuthContext>
+            
         );
     }
 }
