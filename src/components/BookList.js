18ccab089a28e8f6bc343a89ecc09df66d7fdaf1
AleksHodur <hxl82516@educastur.es>
2024-02-06 16:54:35 +0100

    useContext with sfc

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,24 +1,19 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class BookList extends Component {
+const BookList = () => {
+    const { isLightTheme, light, dark } = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark;
 
-    static contextType = ThemeContext;
-
-    render() { 
-        const { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark;
-
-        return (
-            <div className="book-list" style={{color: theme.text, background: theme.bg}}>
+    return ( 
+        <div className="book-list" style={{color: theme.text, background: theme.bg}}>
                 <ul>
                     <li style={{background: theme.ui}}>the way of king</li>
                     <li style={{background: theme.ui}}>the name of the wind</li>
                     <li style={{background: theme.ui}}>the final empire</li>
                 </ul>
-            </div>
-        );
-    }
+        </div>
+     );
 }
  
 export default BookList;
